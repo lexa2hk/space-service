b159b13eb3ce13cd3a78f48dcfc75e4644d040f3
lexa2hk <alesha.smolnikoff@gmail.com>
2022-11-17 09:48:21 +0300

    Task 10 fix cart +truncate

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,29 @@
 
+class Accumulator {
+    constructor(startValue) {
+        this.result = startValue;
+    }
+
+    showResult() {
+        alert(this.result);
+    }
+    
+    getResult(){
+        return this.result;
+    }
+    
+    add(value) {
+        this.result+= int(value);
+    }
+
+    read(){
+        this.result += prompt("Введите число", 0);
+    }
+}
+
+let cardData = new Map();
+
+
 document.addEventListener('keydown', function (event) {
     if (event.code == 'Escape') {
         drawflag = false;
@@ -117,8 +142,8 @@ function draw(ev) {
     div.style.height = '50px';
     div.style.width = '50px';
     div.style.backgroundColor = 'cornflowerblue';
-    div.style.top = (y-20) + 'px';
-    div.style.left = (x-20) + 'px';
+    div.style.top = (y - 20) + 'px';
+    div.style.left = (x - 20) + 'px';
     div.style.zIndex = '1000';
     div.style.borderRadius = '50%';
 
@@ -128,5 +153,119 @@ function draw(ev) {
     setTimeout(function () {
         body[0].removeChild(div);
     }, 1000)
+}
+
+
+function truncate(str, maxlength){
+    if (str.length > maxlength) {
+        return str.slice(0, maxlength - 3) + "…";
+    } else {
+        return str;
+    }
+}
+
+
+let a=Math.floor(Math.random()*10);
+let b=Math.floor(Math.random()*10);
+let sum=a+b;
+document.getElementById("capcha").innerHTML = a + " + " + b + " = ?";
+
+let CapchaBtn = document.getElementById("check-capcha");
+
+
+function isEmpty( val ) {
+
+    // test results
+    //---------------
+    // []        true, empty array
+    // {}        true, empty object
+    // null      true
+    // undefined true
+    // ""        true, empty string
+    // ''        true, empty string
+    // 0         false, number
+    // true      false, boolean
+    // false     false, boolean
+    // Date      false
+    // function  false
+
+        if (val === undefined)
+        return true;
+
+    if (typeof (val) == 'function' || typeof (val) == 'number' || typeof (val) == 'boolean' || Object.prototype.toString.call(val) === '[object Date]')
+        return false;
+
+    if (val == null || val.length === 0)        // null or 0 length array
+        return true;
+
+    if (typeof (val) == "object") {
+        // empty object
+
+        var r = true;
+
+        for (var f in val)
+            r = false;
+
+        return r;
+    }
+
+    return false;
+}
+
+
+let input_capcha = document.getElementById("capcha-input");
+input_capcha.addEventListener("input", function (e) {
+    if(isEmpty(e.target.value)){
+        CapchaBtn.disabled = true;
+        console.log("isEmpty");
+    }
+    if (e.target.value == sum) {
+        document.getElementById("capcha-input").style.backgroundColor = "lightgreen";
+        CapchaBtn.disabled = false;
+    } else {
+        document.getElementById("capcha-input").style.backgroundColor = "pink";
+        CapchaBtn.disabled = true;
+
+    }
+});
+
+
+CapchaBtn.addEventListener("click", function (e) {
+    if (input_capcha.value == sum) {
+        alert("Успешный вход!");
+    } else {
+        alert("Как сделал это?");
+    }
+});
+
+
+
+let accumulatorInstance = new Accumulator(1);
+
+//if not in cart, then add to cart
+//if in cart, increase quantity using add(value) method
+
+
+
+function addToCart(name, price) {
+    console.log(name + " " + price);
+    let cart = document.getElementById("shopping-cart");
+    let cartSearch = document.getElementById(name+"-cart");
+    if (cartSearch == null) {
+        cartItem = document.createElement("div");
+        cartItem.id = name + "-cart";
+        cartItem.className = "cart-item";
+        var tempAcc = new Accumulator(1);
+        cardData.set(name, tempAcc);
+
+        cartItem.innerHTML = name + " " + price + " руб. / " + cardData[name].getResult();
+        
+        console.log(cart.innerHTML);
+        cart.appendChild(cartItem);
+    } else {
+        let el = document.getElementById(name + "-cart");
+        cardData[name].read();
+        el.innerHTML = name + " " + price + " руб. / " + cardData[name].getResult();
+    }
 }
 
